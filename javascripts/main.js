1302be71a934da785b4c525e2ad384ae541226c7
Alex Orga <alexorga.dev@gmail.com>
2018-04-30 20:59:10 -0500

    can append a fish card from available to fish basket

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,9 @@
 // Filter fish that are "on sale"
 
 // Add fish to "Basket"
+const moveTheCart = (e) => {
+  let fishCard = $(e.target).closest('.fish')
+  $('#snagged').append(fishCard);
+};
+
+$('button.add').click(moveTheCart);
